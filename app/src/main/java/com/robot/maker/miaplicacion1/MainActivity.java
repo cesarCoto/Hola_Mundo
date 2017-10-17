030d7535d2d2558f20a604fa98da8290d67cf41e
cesarCoto <cesar.cotobvb@gmail.com>
2017-10-17 01:15:39 -0500

    Aplicacion terminada lector de eventos en Android

--- app/src/main/java/com/robot/maker/miaplicacion1/MainActivity.java
+++ app/src/main/java/com/robot/maker/miaplicacion1/MainActivity.java
@@ -2,12 +2,32 @@ package com.robot.maker.miaplicacion1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btn_click;
+    TextView tv_hola;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btn_click = (Button) findViewById(R.id.button_clickme);
+        tv_hola = (TextView) findViewById(R.id.textView_hola_mundo);
+
+        btn_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tv_hola.setVisibility(View.VISIBLE);
+
+            }
+        });
     }
 }
